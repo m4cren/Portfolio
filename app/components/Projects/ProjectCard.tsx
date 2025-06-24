788691ccec284aa40d561ddda56756fe6821ba69
m4cren <renrenofficial@gmail.com>
2025-06-24 21:35:42 +0800

    fixed cta button y padding and added loading animation on project card

--- app/components/Projects/ProjectCard.tsx
+++ app/components/Projects/ProjectCard.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { ProjectTypes } from "@/app/lib/types";
 import { Eye } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ProjectCardProps {
    item: ProjectTypes;
@@ -9,15 +11,22 @@ interface ProjectCardProps {
 }
 
 const ProjectCard = ({ item, handleSelectedProject }: ProjectCardProps) => {
+   const [isLoaded, setIsLoaded] = useState<boolean>(false);
    return (
       <li className="[background-image:linear-gradient(to_top,var(--color-leather),var(--color-goldenbeige))] w-[18rem] flex flex-col items-center min-w-[18rem] h-[24rem] md:h-[26rem] bg-gradient-to-t from-leather to-goldenbeige rounded-xl [box-shadow:0_0_4px_rgba(0,0,0,0.4)_inset]">
          <Image
             src={`/images/${item.image_url}.png`}
             alt="metafeast"
             width={500}
+            onLoad={() => setIsLoaded(true)}
             height={500}
             className="w-[90%] -mb-4"
          />
+         {!isLoaded && (
+            <div className="absolute inset-0 flex items-center justify-center h-1/2">
+               <span className="loading loading-infinity loading-3xl scale-300 text-softbutter"></span>
+            </div>
+         )}
          <div className="flex flex-col items-center gap-2">
             <h4>{item.label}</h4>
             <p className="text-center text-xs! md:text-sm! w-[80%] md:w-[95%]">
